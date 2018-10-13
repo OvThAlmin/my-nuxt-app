1dc1865f0efa33aeda4042f34d3751835f6d49c6
OvThAlmin <text@example.com>
2018-10-13 20:29:37 +0900

    add first commit for test

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="title">About this App</h1>
+
+    <p>アプリケーションの紹介文がここに入ります。</p>
+
+    <a href="/">トップページへ戻る</a>    
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return { }
+  }  
+}
+</script>
